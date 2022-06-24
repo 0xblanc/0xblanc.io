8b28309b1ed7984f82d2752b1f07f40419b12d3b
Timothy Lam <timothy.lam@cinchblock.com>
2022-06-24 17:08:51 +0800

    update

--- src/containers/ImageShowcase.tsx
+++ src/containers/ImageShowcase.tsx
@@ -1,15 +1,21 @@
 const ImageShowcase = () => {
   return (
-    <div className="h-screen">
-      <div className="w-full justify-center items-center flex flex-col">
+    <div className="h-screen sm:mx-0 xl:mx-64 flex flex-col justify-center">
+      <div className="w-full justify-center items-center flex flex-col text-center">
         <h1 className="text-2xl text-darkBlue ">Want something special? </h1>
         <h1 className="text-5xl text-darkBlue font-bold">
           AI image processed NFT generation
         </h1>
-        <div></div>
       </div>
-      <div className="grid grid-cols-2 w-full h-1/2 my-10 ">
-        <div className="col-span-1 bg-aliceBlue h-full ">1</div>
+      <div className="grid sm:grid-cols-2 w-full h-1/2 my-10  flex-col-reverse sm:flex-col">
+        <div className="col-span-1 bg-aliceBlue h-full flex space-y-10 flex-col justify-end md:justify-start">
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+        </div>
         <div className="col-span-1 bg-white h-full">2</div>
       </div>
     </div>
