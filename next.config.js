c8fe129b292fb6f60537c3dfe44e1e0c1b493381
Sennett Lau <laub1199@gmail.com>
2022-07-04 01:27:19 +0800

    updated docs, added nojekyll

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/0xblanc/0xblanc.io/main/docs/',
   reactStrictMode: false,
   jsconfigPaths: true,
   tsconfigPath: true,
