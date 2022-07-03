0421e47d2c2aa90cae5bb7fac30116f376f1ed74
Sennett Lau <laub1199@gmail.com>
2022-07-04 00:01:41 +0800

    updated docs

---
+++ docs/_next/static/ZJRfnffjhpHPW9xSqikf9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/901-56c8b6d106a21972.js","static/chunks/pages/index-7c9350991091efb2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- docs/_next/static/chunks/pages/components/templates/DesktopFrame-e87631a08813727b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[682],{3340:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/templates/DesktopFrame",function(){return e(9373)}])}},function(n){n.O(0,[664,373,774,888,179],(function(){return _=3340,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
