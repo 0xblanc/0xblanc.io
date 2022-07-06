d55ee91925b0b52e956a6f5933854c3016603060
Sennett Lau <laub1199@gmail.com>
2022-07-06 17:26:27 +0800

    Revert "update hreflang"
    This reverts commit 1b96c852daac81aa06f042c272d56ebf2d7e3ca3.

---
+++ docs/_next/static/LaOoX0Mp4nDUtKYGpKAgj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/67-ca66e0b0bb160487.js","static/chunks/pages/index-977a453b2813408a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
