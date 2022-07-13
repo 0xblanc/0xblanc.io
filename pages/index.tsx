1737f5676b67ef9ac5435e6a3d84c80e5f1ceed6
Sennett Lau <laub1199@gmail.com>
2022-07-13 11:08:01 +0800

    update favicon related meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,10 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>{t('meta.title')}</title>
-        <link rel='icon' href='/logo512.png' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon-precomposed' href='/favicon.ico' />
         <link rel='canonical' href='https://0xblanc.io' />
         <link rel='alternate' href='https://0xblanc.io' hrefLang='en' />
         <link rel='alternate' href='https://0xblanc.io/zh-hk' hrefLang='zh' />
