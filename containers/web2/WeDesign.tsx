c7617475e2d3b5882acb7f58f138d0b6a3aecdbb
Sennett Lau <laub1199@gmail.com>
2022-08-13 23:13:53 +0800

    updated web2hero with responsive design, minor update on WeDesign

--- containers/web2/WeDesign.tsx
+++ containers/web2/WeDesign.tsx
@@ -7,7 +7,7 @@ const WeDesign = (props: any) => {
 
   return (
     <div id='web2-we-design' className='bg-dark'>
-      <div className='container mx-auto flex py-40'>
+      <div className='container mx-auto flex py-40 px-36'>
         <div className='w-1/2 flex justify-center flex-col relative items-center'>
           <div className='z-10'>
             <h2 className='text-white text-8xl font-bold mb-3 w-fit'>UI/UX Design</h2>
@@ -15,7 +15,7 @@ const WeDesign = (props: any) => {
           </div>
           <div className='w-72 aspect-square border-2 border-gold absolute left-72 top-1/2 -translate-y-1/2 -translate-x-1/2 rotate-45 opacity-30'/>
         </div>
-        <div className='w-1/2 flex justify-center'>
+        <div className='w-1/2 flex justify-end'>
           <img className='w-3/5' src="https://images.unsplash.com/photo-1632516643720-e7f5d7d6ecc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1011&q=80" alt="Image" />
         </div>
       </div>
--- containers/web2/Web2Hero.tsx
+++ containers/web2/Web2Hero.tsx
@@ -6,21 +6,22 @@ const Web2Hero = (props: any) => {
   const { t } = useTranslation()
 
   return (
-    <div id='web2-hero' className='h-screen min-h-full w-full bg-cover flex bg-web2-hero'>
-      <div className='flex-1 flex flex-col bg-black bg-opacity-25 px-12 3xl:px-80'>
-        <div className='flex py-10 justify-between items-center'>
+    <div id='web2-hero' className='h-screen min-h-full w-full bg-cover flex bg-web2-hero bg-center'>
+      <div className='flex-1 flex flex-col bg-black bg-opacity-25 px-6 md:px-12 3xl:px-80'>
+        <div className='flex flex-col md:flex-row pb-0 md:pb-10 pt-20 md:pt-10 justify-between items-center'>
           <img
+            className='mb-8 md:mb-0 w-80 md:w-auto'
             src='https://res.cloudinary.com/blanc-io/image/upload/v1656857424/website/text_logo_gold_no01vb.svg'
             alt='Logo'
             width={196}
             height={41}
           />
-          <p className='text-gold font-bold text-2xl'>
+          <p className='text-gold font-bold text-2xl text-end'>
             {t('Web2Hero.subtitle')}
           </p>
         </div>
-        <div className='flex-1 flex items-center overflow-clip relative '>
-          <h1 className='text-white font-bold text-ul' dangerouslySetInnerHTML={{ __html: t('Web2Hero.slogan')}} />
+        <div className='flex-1 flex items-center overflow-clip relative justify-center md:justify-start'>
+          <h1 className='text-white font-bold text-5xl mb-24 md:mb-0 md:text-9xl lg:text-ul' dangerouslySetInnerHTML={{ __html: t('Web2Hero.slogan')}} />
           {/*<div className='flex absolute -translate-y-[5rem] right-40 overflow-visible'>*/}
           {/*  <Cube />*/}
           {/*</div>*/}
