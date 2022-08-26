7cf807dcd90043f89b14e8dfb9a1909b9a25a9a0
Sennett Lau <laub1199@gmail.com>
2022-08-27 02:37:16 +0800

    updated Web2Hero

--- containers/web2/Web2Hero.tsx
+++ containers/web2/Web2Hero.tsx
@@ -7,26 +7,26 @@ const Web2Hero = (props: any) => {
 
   return (
     <div id='web2-hero' className='h-screen min-h-full w-full bg-cover flex bg-web2-hero bg-center'>
-      <div className='flex-1 flex flex-col bg-black bg-opacity-25 px-6 md:px-12 3xl:px-80'>
-        <div className='flex flex-col md:flex-row pb-0 md:pb-10 pt-40 md:pt-10 justify-between items-center'>
+        <div className='flex-1 flex flex-col items-center overflow-clip relative justify-center pb-[86px]'>
           <img
-            className='mb-8 md:mb-0 w-80 md:w-auto'
-            src='https://res.cloudinary.com/blanc-io/image/upload/v1656857424/website/text_logo_gold_no01vb.svg'
+            className='mb-4'
+            src='https://res.cloudinary.com/blanc-io/image/upload/q_auto/v1661538702/website/text_logo_white_r06vnu.svg'
             alt='Logo'
             width={196}
             height={41}
           />
-          <p className='text-gold font-bold text-2xl text-end'>
+          <p className='text-white text-xl text-end mb-4'>
             {t('Web2Hero.subtitle')}
           </p>
+          <div className='reflection'>
+            <h1 className='text-white text-4xl md:text-6xl'>
+              {t('Web2Hero.slogan')}
+            </h1>
+            <h1 className='text-white text-4xl md:text-6xl'>
+              {t('Web2Hero.slogan')}
+            </h1>
+          </div>
         </div>
-        <div className='flex-1 flex items-center overflow-clip relative justify-center md:justify-start'>
-          <h1 className='text-white font-bold text-5xl mb-24 md:mb-0 md:text-9xl lg:text-ul' dangerouslySetInnerHTML={{ __html: t('Web2Hero.slogan')}} />
-          {/*<div className='flex absolute -translate-y-[5rem] right-40 overflow-visible'>*/}
-          {/*  <Cube />*/}
-          {/*</div>*/}
-        </div>
-      </div>
     </div>
   )
 }
