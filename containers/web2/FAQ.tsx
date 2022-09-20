cf541ca13700b489df63c716374d619335d61a9b
Sennett Lau <laub1199@gmail.com>
2022-09-20 12:03:18 +0800

    minor styling update for faq

--- containers/web2/FAQ.tsx
+++ containers/web2/FAQ.tsx
@@ -61,7 +61,7 @@ const FAQ = (props: any) => {
         {/*<div className={'container w-full mx-auto flex justify-center'}>*/}
         {/*  <Title text={t('web2.faq.title')} mode={'dark'} />*/}
         {/*</div>*/}
-        <ul className='flex flex-col justify-center px-4 md:px-0 w-full pt-7 md:pb-12 max-w-[992px] mx-auto -translate-x-60'>
+        <ul className='flex flex-col justify-center px-4 md:px-0 w-full pt-7 md:pb-12 max-w-[700px] mx-auto -translate-x-60'>
           {faqs.map((faq, index) => (
             <li key={index} className={`flex w-full py-5 border-gold flex-col cursor-pointer ${index < faqs.length - 1 ? 'border-b-2' :''}`} onClick={() => expandHandler(index)}>
               <div className='flex'>
@@ -87,7 +87,7 @@ const FAQ = (props: any) => {
             </li>
           ))}
         </ul>
-        <div className='aspect-square w-[300px] border-2 border-gold rotate-45 absolute right-[150px] top-[-10px]'>
+        <div className='aspect-square w-[300px] border-2 border-gold rotate-45 absolute right-[100px] top-[-10px]'>
           <div className='w-full h-full flex justify-center items-center relative -rotate-90'>
             <h2 className='text-6xl text-white rotate-45'>{t('web2.faq.title')}</h2>
             <div className='custom-gradient w-[3000px] h-[2px] absolute bottom-[-175px] left-[-350px]'></div>
